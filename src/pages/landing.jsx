ea9de4ad5680bc6cf2344a9a3446c6b466e7c98a
Rishi Kothari <itsrishikothari@gmail.com>
2019-12-10 10:44:34 -0500

    Architecture

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,43 +1,90 @@
-import {Box, Image, Heading, Text, Grommet, Button} from 'grommet';
+import {
+  Box,
+  Image,
+  Heading,
+  Text,
+  Grommet,
+  Button,
+  Chart,
+  WorldMap
+} from "grommet";
 import Typist from "react-typist";
 import React from "react";
 
 const LandingPage = props => {
   return (
     <>
-
-      <Box justify={"center"} align={"center"}>
-      <Box justify={"center"} align={"center"} responsive animation={"fadeIn"} direction={"row-responsive"} pad="large">
-
-        <Box justify={"center"} width={"medium"} pad={"none"} height={"medium"}>
-          <Image
-              fit="cover"
-              src={"https://i.ibb.co/3sL75R7/lilypad.png"}
-          />
-        </Box>
-        <Box pad={"medium"}/>
+      <Box animation={"fadeIn"} pad="small">
+        <Box
+          justify={"center"}
+          align={"center"}
+          responsive
+          direction={"row-responsive"}
+          pad="large"
+        >
+          <Box
+            justify={"center"}
+            width={"medium"}
+            pad={"none"}
+            height={"medium"}
+          >
+            <Image fit="cover" src={"https://i.ibb.co/3sL75R7/lilypad.png"} />
+          </Box>
+          <Box pad={"medium"} />
 
           <Box>
             <Heading size={"small"}>
-            <Typist>
-              We are
-              <Typist.Backspace count={6} delay={1000} />
-              You are
-              <Typist.Backspace count={7} delay={1000} />
-              Welcome to
-
-            </Typist>
+              <Typist>Welcome to</Typist>
             </Heading>
-            <Heading margin={"none"} size={"xlarge"}>Lilypad.</Heading>
-            <Text size={"medium"} style={{"margin-top":"1em"}} >Your centre for carbon savings.</Text>
-            <Box pad={"small"}/>
+            <Heading margin={"none"} size={"xlarge"}>
+              Lilypad.
+            </Heading>
+            <Text size={"medium"} style={{ "margin-top": "1em" }}>
+              Your centre for carbon savings.
+            </Text>
+            <Box pad={"small"} />
             <Button
-                label={<Heading size={"small"}>Sign in</Heading>}
-                primary
-                onClick={props.signIn}
+              label={<Heading size={"small"}>Sign in</Heading>}
+              primary
+              color="light-1"
+              onClick={props.signIn}
             />
           </Box>
-      </Box>
+        </Box>
+        <Box></Box>
+        <Box justify="center">
+          <Heading alignSelf="center">Community</Heading>
+          <WorldMap
+            alignSelf="center"
+            justify="center"
+            alignmentBaseline="central"
+            color="brand"
+            continents={[
+              {
+                name: "Africa",
+                color: "light-5",
+                onClick: name => {
+                  alert(name);
+                }
+              }
+            ]}
+            onSelectPlace={(lat, lon) => {
+              alert(lat);
+            }}
+            places={[
+              {
+                name: "Sydney",
+                location: [-33.8830555556, 151.216666667],
+                color: "accent-2",
+                onClick: name => {
+                  alert(name);
+                }
+              }
+            ]}
+            margin="none"
+            fill="horizontal"
+          />
+        </Box>
       </Box>
     </>
   );
